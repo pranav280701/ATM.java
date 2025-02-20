6bf0603258b0ea04b9462fd73857d285f363181e
Pranv Chandanshive <pranavchandanshive28@gmail.com>
2025-02-20 10:58:09 +0530

    Source code added

---
+++ BankSystem.java
@@ -0,0 +1,17 @@
+class BankSystem{ 
+
+public static void main(String[] args) {
+        BankAccount myAccount = new BankAccount(10000);
+        
+        System.out.println("Current Balance: " + myAccount.getBalance());
+        
+        if (myAccount.getCash(2000)) {
+            System.out.println("Withdrawal Successful. New Balance: " + myAccount.getBalance());
+        } else {
+            System.out.println("Insufficient funds!");
+        }
+        
+        myAccount.depositCash(5000);
+        System.out.println("After Deposit, Balance: " + myAccount.getBalance());
+    }
+}
