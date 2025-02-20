f316696ada9ef7977a67e12f6bd5cd61c4c46957
Pranv Chandanshive <pranavchandanshive28@gmail.com>
2025-02-20 11:02:19 +0530

    Source code added

--- BankAccount.java
+++ BankAccount.java
@@ -1,7 +1,42 @@
-class BankAccount{
-public static void main(String [] nas){
+class BankAccount implements ATM {
+    private double balance;
+    private static final String LOG_FILE = "transaction_log.txt";
 
-System.out.println("This is your Bank Account");
-}
+    public BankAccount(double initialDeposit) {
+        this.balance = initialDeposit;
+        logTransaction("Account created with balance: " + balance);
+    }
 
+    @Override
+    public double getBalance() {
+        logTransaction("Checked balance: " + balance);
+        return balance;
+    }
+
+    @Override
+    public boolean getCash(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            logTransaction("Withdrawn: " + amount + ", Remaining Balance: " + balance);
+            return true;
+        } else {
+            logTransaction("Failed withdrawal attempt: " + amount);
+            return false;
+        }
+    }
+    
+    public void depositCash(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            logTransaction("Deposited: " + amount + ", New Balance: " + balance);
+        }
+    }
+
+    private void logTransaction(String message) {
+        try (PrintWriter writer = new PrintWriter(new FileWriter(LOG_FILE, true))) {
+            writer.println(message);
+        } catch (IOException e) {
+            System.out.println("Error logging transaction: " + e.getMessage());
+        }
+    }
 }
